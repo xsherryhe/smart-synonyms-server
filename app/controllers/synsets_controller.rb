2ca6feaf83d7111e354fb3663748d62781de5fca
Sherry He <xsherryhe@gmail.com>
2023-04-03 15:24:03 -0400

    Update synsets controller to match updates to synset model

--- app/controllers/synsets_controller.rb
+++ app/controllers/synsets_controller.rb
@@ -1,6 +1,6 @@
 class SynsetsController < ApplicationController
   def show
     @synset = Synset.new(params[:pos], params[:pos_offset].to_i)
-    render json: @synset, exclude_from_synonyms: params[:word]
+    render json: @synset.as_json_with_synonyms(exclude_from_synonyms: params[:word])
   end
 end
