4199d66cf4bd40ffc8513cb60c917474d8622af6
Sherry He <xsherryhe@gmail.com>
2023-04-03 15:24:50 -0400

    Rewrite word model to better structure and expose word and glosses

--- app/models/word.rb
+++ app/models/word.rb
@@ -1,41 +1,53 @@
 class Word
   def initialize(word)
-    @lemmas = WordNet::Lemma.find_all(word)
+    initialize_lemmas_and_adjusted_word(word)
   end
 
   def glosses
     return [] if @lemmas.empty?
 
-    filter_mapped_glosses = @lemmas.filter_map do |lemma|
-      lemma.pos == 'r' ? nil : lemma_glosses(lemma)
-    end
-    filter_mapped_glosses.any? ? filter_mapped_glosses : adj_glosses
+    @adverb_only ? adj_glosses : word_glosses
   end
 
-  def as_json(_ = {})
-    glosses
+  def as_json(_options = {})
+    { word: @adjusted_word, glosses: }
   end
 
   private
 
-  def lemma_glosses(lemma)
-    synsets = lemma.synsets.first(3)
-    {
-      id: lemma.pos + synsets.first.pos_offset.to_s,
-      word: lemma.word,
-      pos: lemma.pos,
-      synsets: synsets.map do |synset|
-        { pos_offset: synset.pos_offset, gloss: synset.gloss }
-      end
-    }
+  def initialize_lemmas_and_adjusted_word(word)
+    [word, word.downcase, word.downcase.capitalize].each do |possible_word|
+      @adjusted_word = possible_word
+      @lemmas = Lemma.find_all(@adjusted_word)
+      break if @lemmas.any?
+    end
+    handle_adverb_only_case
+  end
+
+  def handle_adverb_only_case
+    @adverb_only = @lemmas.all? { |lemma| lemma.pos == 'r' }
+    return unless @adverb_only
+
+    @adj_synset = @lemmas.first.synsets.first.relation('\\').first
+    @adjusted_word = @adj_synset.words.first
+  end
+
+  def word_glosses
+    @lemmas.filter_map do |lemma|
+      next if lemma.pos == 'r'
+
+      synsets = lemma.synsets.first(3)
+      {
+        id: lemma.pos + synsets.first.pos_offset.to_s, pos: lemma.pos,
+        synsets: synsets.map(&:as_json)
+      }
+    end
   end
 
   def adj_glosses
-    adj_synset = @lemmas.first.synsets.first.relation('\\').first
     [{
-      word: adj_synset.words.first,
-      pos: adj_synset.pos,
-      synsets: [{ pos_offset: adj_synset.pos_offset, gloss: adj_synset.gloss }]
+      id: @adj_synset.pos + @adj_synset.pos_offset.to_s, pos: @adj_synset.pos,
+      synsets: [@adj_synset.as_json]
     }]
   end
 end
