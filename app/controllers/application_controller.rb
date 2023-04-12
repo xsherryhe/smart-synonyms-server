92e41eff7f7d25fe12450d747e27c5c75d443264
Sherry He <xsherryhe@gmail.com>
2023-04-11 23:46:20 -0400

    Add application#init route for pinging server

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::API
+  def init
+    head :ok
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'init', to: 'application#init'
   get 'words/:word', to: 'words#show'
   get 'synsets/:pos/:pos_offset', to: 'synsets#show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
