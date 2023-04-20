a5c4a615ca43c023ac83ebf7e00d374be031b9a6
Sherry He <xsherryhe@gmail.com>
2023-04-20 19:45:12 -0400

    Begin writing tests for Word class

---
+++ test/models/word_test.rb
@@ -0,0 +1,74 @@
+require 'test_helper'
+
+class WordTest < ActiveSupport::TestCase
+  describe '#word' do
+    it 'returns the input word when lemmas of the input word are found' do
+      Lemma.stub :find_all, lambda { |word|
+        word == 'aBc' ? Array.new(3) { MockLemma.new } : []
+      } do
+        word_data = Word.new('aBc')
+        word = word_data.word
+        assert_equal('aBc', word)
+      end
+    end
+
+    it 'returns the lowercase input word when lemmas of the input word in lowercase are found' do
+      Lemma.stub :find_all, lambda { |word|
+        word == 'abc' ? Array.new(3) { MockLemma.new } : []
+      } do
+        word_data = Word.new('aBc')
+        word = word_data.word
+        assert_equal('abc', word)
+      end
+    end
+
+    it 'returns the capitalized input word when lemmas of the capitalized input word are found' do
+      Lemma.stub :find_all, lambda { |word|
+        word == 'Abc' ? Array.new(3) { MockLemma.new } : []
+      } do
+        word_data = Word.new('aBc')
+        word = word_data.word
+        assert_equal('Abc', word)
+      end
+    end
+
+    it 'returns nil when no version of the input word is found as a lemma' do
+      Lemma.stub :find_all, ->(_word) { [] } do
+        word_data = Word.new('aBc')
+        word = word_data.word
+        assert_nil(word)
+      end
+    end
+  end
+
+  ## Glosses -- returns array of hashes with id, pos, synsets array
+    ## Test non-adverb
+    ## Test adverb -- stub MockLemma#pos
+  # describe 'Word#glosses' do
+  #   setup do
+  #     Lemma.stub :find_all, Minitest::Mock.new do
+  #       Array.new(3) { MockLemma.new }
+  #     end
+  #   end
+
+  #   describe 'when the word is not an adverb' do
+  #     it 'returns an array' do
+  #       word_data = Word.new('apple')
+  #       glosses = word_data.glosses
+  #       assert_instance_of(Array, glosses)
+  #     end
+
+  #     it 'returns an array of hashes having a string id' do
+  #       word_data = Word.new('apple')
+  #       id = word_data.glosses.first.id
+  #       assert_instance_of(String, id)
+  #     end
+
+  #     it 'returns an array of hashes having a pos property' do
+  #       word_data = Word.new('apple')
+  #       id = word_data.glosses.first.id
+  #       assert_includes(%w[n v a], id)
+  #     end
+  #   end
+  # end
+end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,8 @@
 ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
+require 'minitest/spec'
+require 'minitest/mock'
 
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
@@ -10,4 +12,20 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+end
+
+class MockSynset
+  def as_json
+    %w[a b c d e 1 2 3].sample
+  end
+end
+
+class MockLemma
+  def pos
+    %w[n v a].sample
+  end
+
+  def synsets
+    Array.new(3) { MockSynset.new }
+  end
 end
